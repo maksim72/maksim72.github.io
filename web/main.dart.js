38ab598aaccc5e2837671c6af3d32667412f48ff
Maksim Pischulenok <pischule@outlook.com>
2021-04-06 23:20:05 +0300

    fix 3

--- web/main.dart.js
+++ web/main.dart.js
@@ -21330,6 +21330,7 @@ CC:function(a,b){if(!$.a6O){$.a6O=!0
 window
 if(typeof console!="undefined")window.console.warn("The performance overlay isn't supported on the web")}},
 CD:function(a,b,c,d){var s,r
+c
 t.l9.a(b)
 s=b.a.b
 r=new H.ed(null)
@@ -56101,4 +56102,3 @@ a(b.target)}for(var r=0;r<s.length;++r)s[r].addEventListener("load",onLoad,false
 var s=F.a0j
 if(typeof dartMainRunner==="function")dartMainRunner(s,[])
 else s([])})})()
-//# sourceMappingURL=main.dart.js.map
